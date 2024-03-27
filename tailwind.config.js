3671765f42a9f1261f922d4f4a3a5bb687dfd501
Anushree Jha <anushreejha0604@gmail.com>
2024-03-27 22:55:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-// tailwind.config.js
-
 module.exports = {
   content: [
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
-    // Add other paths to your content sources as needed
   ],
   theme: {
     extend: {},
