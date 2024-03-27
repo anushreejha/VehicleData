872bd1297172b73e7fe18793402951d2d2a7ec60
Anushree Jha <anushreejha0604@gmail.com>
2024-03-27 19:35:49 +0530

    bad updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    // Add other paths to your content sources as needed
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
